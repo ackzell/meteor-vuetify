7105661162b84eb07d7ffd4fdfbf6c74ead11db3
ackzell <ackzell@gmail.com>
2018-06-15 08:21:06 -0500

    Adding prettier to the vscode config.

--- client/main.js
+++ client/main.js
@@ -1,8 +1,8 @@
-import { Meteor } from "meteor/meteor";
-import { Vue } from "meteor/akryum:vue";
+import { Meteor } from 'meteor/meteor'
+import Vue from 'vue'
 
-import App from '/imports/ui/App.vue';
+import App from '/imports/ui/App.vue'
 
 Meteor.startup(() => {
-  new Vue(App).$mount(document.body);
-});
+  new Vue(App).$mount(document.body)
+})
