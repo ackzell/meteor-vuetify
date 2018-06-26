d2bbbfe29a6b7828c08788416c3b389797b7f76f
ackzell <ackzell@gmail.com>
2018-06-26 08:29:48 -0500

    Fixing import error. Ignoring the 'documentation' html. Addresses #1

---
+++ imports/ui/auth/Logout.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-btn @click="logout">Logout</v-btn>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      this.$store.dispatch('logout')
+    }
+  }
+}
+</script>
